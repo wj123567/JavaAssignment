cb66cff35445a372af524bffe15c0bffc4fcd775
Thomas Lim <90422186+thomaslimfc@users.noreply.github.com>
2024-03-13 16:11:39 +0800

    update

--- WhatSon/src/whatson/WhatSon.java
+++ WhatSon/src/whatson/WhatSon.java
@@ -1,11 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package whatson;
 public class WhatSon {
     public static void main(String[] args) {
-        hifewhfiewhifehi
+        System.out.println("Hello, What son!");
+        
     }
     
 }
