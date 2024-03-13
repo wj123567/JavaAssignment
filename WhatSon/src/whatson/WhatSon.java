e6aa8760fd2e6478d797564e75842cc3e0aab6c7
wj123567 <weijia0811@gmail.com>
2024-03-13 15:57:20 +0800

    update

--- WhatSon/src/whatson/WhatSon.java
+++ WhatSon/src/whatson/WhatSon.java
@@ -3,18 +3,9 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package whatson;
-
-/**
- *
- * @author User
- */
 public class WhatSon {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
     }
     
 }
